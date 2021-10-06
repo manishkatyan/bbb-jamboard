caaba2c3735896146ad083ad4aa5fb4f947b2bbd
Arunkumar <arun@higheredlab.com>
2021-10-06 09:53:54 +0530

    Small improvement  in detecting click

--- bbb-jamboard.js
+++ bbb-jamboard.js
@@ -11,9 +11,9 @@ const checkElement = async selector => {
     if(layout){
       var element = document.querySelector('ul[class^=MuiList-root]');
       var li = document.createElement("li")
-      var div = document.createElement("div")
-      var i = document.createElement("i")
       var a = document.createElement("a")
+      var i = document.createElement("i")
+      var div = document.createElement("div")
     
       li.classList = "MuiButtonBase-root MuiListItem-root MuiMenuItem-root menuitem--Zqo0Cv  MuiListItem-button"
       li.setAttribute = ("tabindex", "-1")
@@ -21,23 +21,18 @@ const checkElement = async selector => {
       li.setAttribute = ("aria-disabled", "false")
       li.style.cssText = "padding: 8px 4px; margin-left: 4px; margin-right: 4px"
     
-      div.style.cssText = "display: flex; flex-flow: row; width: 100%;"
-    
-      i.classList = "icon-bbb-desktop"
-    
-      a.classList = "option--RgzMA"
-      a.style.cssText = "text-decoration:none;"
+      a.style.cssText = "display: flex; flex-flow: row; width: 100%;text-decoration:none;"
       a.href = "https://jamboard.google.com/"
       a.onclick = function(){
         alert('Starting Google Jamboard in a new tab. Please share your screen for attendees to view your Jamboard.')
       }
       a.setAttribute('target', '_blank');
-      a.innerText = "Open Jamboard"
-    
-    
-    div.appendChild(i)
-    div.appendChild(a)
-    li.appendChild(div)
+      i.classList = "icon-bbb-desktop"
+      div.innerText = "Open Jamboard"
+      div.classList = "option--RgzMA"
+    a.appendChild(i)
+    a.appendChild(div)
+    li.appendChild(a)
     element.appendChild(li)
     }
     
